f9ca2171823113a747751030b8e059d93420b893
godeeyeon <godeeyeon@gmail.com>
2024-06-16 13:31:31 +0900

    SPR-57 feat: Layout과 Navigation 컴포넌트 작성 - done: Layout 완료, Navigation 웹 기준 완료
    - need: active 창 CSS 변화
    - 모바일 화면 햄버거 메뉴
    - Icon 클릭 시 페이지 라우팅 혹은 모달 기능 추가

--- front/shoe-prima-frontend/src/components/common/Layout.tsx
+++ front/shoe-prima-frontend/src/components/common/Layout.tsx
@@ -1,5 +1,32 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import Navigation from './Navigation';
+
+const Layout: React.FC = () => {
+  return (
+    <Wrapper>
+      <Navigation />
+      <Pages>
+        <Outlet />
+      </Pages>
+    </Wrapper>
+  );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
+
+const Pages = styled.div`
+  box-sizing: border-box;
+  max-width: 1280px;
+  width: 100%;
+
+  padding: 2.5rem;
+`;
