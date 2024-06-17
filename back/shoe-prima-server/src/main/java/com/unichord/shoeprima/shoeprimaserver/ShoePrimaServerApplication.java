073ecf55d9f9e81fc458200bdadf521436b99350
Glenn-syj <glenn.syj@gmail.com>
2024-06-17 13:51:07 +0900

    SPR-16 init: Spring Boot 폴더 초기화
    - done: application.yml 파일 및 세부 파일 작성
    - done: 도메인형 패키지 구조 적용
    
    - need: 도메인형 패키지 구조 개선

---
+++ back/shoe-prima-server/src/main/java/com/unichord/shoeprima/shoeprimaserver/ShoePrimaServerApplication.java
@@ -0,0 +1,13 @@
+package com.unichord.shoeprima.shoeprimaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoePrimaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoePrimaServerApplication.class, args);
+	}
+
+}
